ce32353451f3cc1e92f2faff83a5c0c2877e08c4
Mao JunJie <mjunjie@uwaterloo.ca>
2025-02-27 23:22:13 -0500

    update premake5

--- premake5.lua
+++ premake5.lua
@@ -7,20 +7,20 @@ project "Imgui"
     objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
 
 
-	files
-	{
-		"imconfig.h",
-		"imgui.h",
-		"imgui.cpp",
-		"imgui_draw.cpp",
-		"imgui_internal.h",
-		"imgui_tables.cpp",
-		"imgui_widgets.cpp",
-		"imstb_rectpack.h",
-		"imstb_textedit.h",
-		"imstb_truetype.h",
-		"imgui_demo.cpp"
-	}
+    files
+    {
+       "imconfig.h",
+       "imgui.h",
+       "imgui.cpp",
+       "imgui_draw.cpp",
+       "imgui_internal.h",
+       "imgui_tables.cpp",
+       "imgui_widgets.cpp",
+       "imstb_rectpack.h",
+       "imstb_textedit.h",
+       "imstb_truetype.h",
+       "imgui_demo.cpp"
+    }
 
     filter "system:windows"
         cppdialect "C++17"
